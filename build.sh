5a55375045294553a124b1a19f6a255407c5d4be
Paul Picazo <ppicazo@gmail.com>
2012-10-01 23:35:13 -0700

    Keep the OTF and TTF artifacts in their own folders. Remove -sp option to makeotf as no one, including me, cares about the fpr files.

--- build.sh
+++ build.sh
@@ -7,18 +7,20 @@ italicWeights=('BlackIt', 'BoldIt', 'ExtraLightIt', 'LightIt', 'It', 'SemiboldIt
 # clean existing build artifacts
 rm -rf target/
 mkdir target/
+mkdir target/OTF/
+mkdir target/TTF/
 
 for w in ${romanWeights[@]};
 do
-  makeotf -sp target/$family-$w-otf.fpr -f Roman/$w/font.pfa -r -o target/$family-$w.otf
-  makeotf -sp target/$family-$w-ttf.fpr -f Roman/$w/font.ttf -gf GlyphOrderAndAliasDB_TT -r -o target/$family-$w.ttf
+  makeotf -f Roman/$w/font.pfa -r -o target/OTF/$family-$w.otf
+  makeotf -f Roman/$w/font.ttf -gf GlyphOrderAndAliasDB_TT -r -o target/TTF/$family-$w.ttf
   rm Roman/$w/current.fpr # remove default options file from the source tree after building
 done
 
 for w in ${italicWeights[@]};
 do
-  makeotf -sp target/$family-$w-otf.fpr -f Italic/$w/font.pfa -r -o target/$family-$w.otf
-  makeotf -sp target/$family-$w-ttf.fpr -f Italic/$w/font.ttf -gf GlyphOrderAndAliasDB_TT -r -o target/$family-$w.ttf
+  makeotf -f Italic/$w/font.pfa -r -o target/OTF/$family-$w.otf
+  makeotf -f Italic/$w/font.ttf -gf GlyphOrderAndAliasDB_TT -r -o target/TTF/$family-$w.ttf
   rm Italic/$w/current.fpr # remove default options file from the source tree after building
 done
 
