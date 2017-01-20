e0da15b3e71211268047d628cd9c1a9d31277d3b
Miguel Sousa <msousa@adobe.com>
2017-01-20 01:08:47 -0800

    refactoring of master feature files

--- buildVFs.sh
+++ buildVFs.sh
@@ -1,2 +1,5 @@
 buildMasterOTFs RomanMasters/SourceSansPro.designspace
-buildCFF2VF RomanMasters/SourceSansPro.designspace  SourceSansPro-Variable.otf
+buildCFF2VF RomanMasters/SourceSansPro.designspace  RomanMasters/SourceSansPro-Variable.otf
+
+rm *Masters/master_*/current.fpr
+rm *Masters/master_*/master.otf
