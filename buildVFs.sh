f4933cfa75b406f94c1f70d57795cbff1559333f
Miguel Sousa <msousa@adobe.com>
2017-01-20 01:39:46 -0800

    Italic masters ready for variable font building

--- buildVFs.sh
+++ buildVFs.sh
@@ -1,5 +1,8 @@
 buildMasterOTFs RomanMasters/SourceSansPro.designspace
 buildCFF2VF RomanMasters/SourceSansPro.designspace  RomanMasters/SourceSansPro-Variable.otf
 
+buildMasterOTFs ItalicMasters/SourceSansPro-Italic.designspace
+buildCFF2VF ItalicMasters/SourceSansPro-Italic.designspace  ItalicMasters/SourceSansPro-VariableItalic.otf
+
 rm *Masters/master_*/current.fpr
 rm *Masters/master_*/master.otf
