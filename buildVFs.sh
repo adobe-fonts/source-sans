ffe039b0e590d1bbbbfb7341ac3a5882cce897dc
Miguel Sousa <msousa@adobe.com>
2017-07-16 23:50:28 -0700

    Use hhea table from OTFs as well

--- buildVFs.sh
+++ buildVFs.sh
@@ -21,11 +21,11 @@ sfntedit -a CFF2=$itm/.tb_cff2 $itm/SourceSansVariable-Italic.otf
 fontmake -m $rom/SourceSansVariable-Roman.designspace -o variable --production-names
 fontmake -m $itm/SourceSansVariable-Italic.designspace -o variable --production-names
 
-# use DSIG, name, OS/2, and STAT tables from OTFs
-sfntedit -x DSIG=$rom/.tb_DSIG,name=$rom/.tb_name,OS/2=$rom/.tb_os2,STAT=$rom/.tb_STAT $rom/SourceSansVariable-Roman.otf
-sfntedit -a DSIG=$rom/.tb_DSIG,name=$rom/.tb_name,OS/2=$rom/.tb_os2,STAT=$rom/.tb_STAT $rom/SourceSansVariable-Roman.ttf
-sfntedit -x DSIG=$itm/.tb_DSIG,name=$itm/.tb_name,OS/2=$itm/.tb_os2,STAT=$itm/.tb_STAT $itm/SourceSansVariable-Italic.otf
-sfntedit -a DSIG=$itm/.tb_DSIG,name=$itm/.tb_name,OS/2=$itm/.tb_os2,STAT=$itm/.tb_STAT $itm/SourceSansVariable-Italic.ttf
+# use DSIG, name, OS/2, hhea, and STAT tables from OTFs
+sfntedit -x DSIG=$rom/.tb_DSIG,name=$rom/.tb_name,OS/2=$rom/.tb_os2,hhea=$rom/.tb_hhea,STAT=$rom/.tb_STAT $rom/SourceSansVariable-Roman.otf
+sfntedit -a DSIG=$rom/.tb_DSIG,name=$rom/.tb_name,OS/2=$rom/.tb_os2,hhea=$rom/.tb_hhea,STAT=$rom/.tb_STAT $rom/SourceSansVariable-Roman.ttf
+sfntedit -x DSIG=$itm/.tb_DSIG,name=$itm/.tb_name,OS/2=$itm/.tb_os2,hhea=$itm/.tb_hhea,STAT=$itm/.tb_STAT $itm/SourceSansVariable-Italic.otf
+sfntedit -a DSIG=$itm/.tb_DSIG,name=$itm/.tb_name,OS/2=$itm/.tb_os2,hhea=$itm/.tb_hhea,STAT=$itm/.tb_STAT $itm/SourceSansVariable-Italic.ttf
 
 # use cmap, GDEF, GPOS, and GSUB tables from TTFs
 sfntedit -x cmap=$rom/.tb_cmap,GDEF=$rom/.tb_GDEF,GPOS=$rom/.tb_GPOS,GSUB=$rom/.tb_GSUB $rom/SourceSansVariable-Roman.ttf
